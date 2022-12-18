0e16c60b79102f1373b1034433c246b073170466
jokerxin <jxpro@qq.com>
2022-12-19 01:37:40 +0800

    Initial state

---
+++ src/main/java/com/jokerxin/x509ca/X509CaApplication.java
@@ -0,0 +1,13 @@
+package com.jokerxin.x509ca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class X509CaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(X509CaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jokerxin/x509ca/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.jokerxin.x509ca.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello World!";
+	}
+}
