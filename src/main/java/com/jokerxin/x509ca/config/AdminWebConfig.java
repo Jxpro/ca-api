3a7578a399f587c4a563c66e07e9b5bbde20dc55
jokerxin <jxpro@qq.com>
2022-12-20 01:16:07 +0800

    Update interceptor to resolve cors error

--- src/main/java/com/jokerxin/x509ca/config/AdminWebConfig.java
+++ src/main/java/com/jokerxin/x509ca/config/AdminWebConfig.java
@@ -9,6 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class AdminWebConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**").excludePathPatterns("/user/login");
+		registry.addInterceptor(new LoginInterceptor())
+				.addPathPatterns("/**")
+				.excludePathPatterns("/user/login","/user/register");
 	}
 }
--- src/main/java/com/jokerxin/x509ca/interceptor/LoginInterceptor.java
+++ src/main/java/com/jokerxin/x509ca/interceptor/LoginInterceptor.java
@@ -8,6 +8,11 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+		if (request.getMethod().equals("OPTIONS")) {
+			response.setHeader("Access-Control-Allow-Origin", "*");
+			response.setHeader("Access-Control-Allow-Headers", "*");
+			return false;
+		}
 		String token = request.getHeader("Authorization");
 		if (token == null || token.equals("")) {
 			response.setStatus(401);
