67554e2294729c80181a51858fe6ddd9078890db
jokerxin <jxpro@qq.com>
2022-12-22 00:12:41 +0800

    Add datasource configuration

--- src/test/java/com/jokerxin/x509ca/X509CaApplicationTests.java
+++ src/test/java/com/jokerxin/x509ca/X509CaApplicationTests.java
@@ -2,14 +2,24 @@ package com.jokerxin.x509ca;
 
 import com.jokerxin.x509ca.utils.Hash;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @SpringBootTest
 class X509CaApplicationTests {
+	@Autowired
+	private DataSource dataSource;
 
 	@Test
 	void testHash() {
 		System.out.println(Hash.sha256("admin"));
 	}
 
+	@Test
+	void testDatabaseConnection() throws SQLException {
+		System.out.println(dataSource.getConnection());
+	}
 }
