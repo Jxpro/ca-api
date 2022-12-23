7daa720eb7346016b946412d80a0ce1ded480574
jokerxin <jxpro@qq.com>
2022-12-23 22:12:00 +0800

    Add @Mapper to user mapper to avoid ide errors

--- src/main/java/com/jokerxin/x509ca/mapper/UserMapper.java
+++ src/main/java/com/jokerxin/x509ca/mapper/UserMapper.java
@@ -2,7 +2,9 @@ package com.jokerxin.x509ca.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.jokerxin.x509ca.entity.User;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 
 }
