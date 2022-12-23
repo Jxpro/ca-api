0d78dd5bc0544f1d2ff70b51cfc5f9e9d261b53b
jokerxin <jxpro@qq.com>
2022-12-23 22:08:57 +0800

    Fix some bugs in user entity

--- src/main/java/com/jokerxin/x509ca/entity/User.java
+++ src/main/java/com/jokerxin/x509ca/entity/User.java
@@ -1,13 +1,16 @@
 package com.jokerxin.x509ca.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 @Data
 public class User {
-    private int id;
+    @TableId(type = IdType.AUTO)
+    private Integer id;
     private String nickname;
     private String username;
     private String password;
     private String email;
-    private byte authority;
+    private Byte authority;
 }
