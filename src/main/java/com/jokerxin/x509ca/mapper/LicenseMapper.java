0cb17566453f3a3c4e4dffa12bce00049dd75f66
jokerxin <jxpro@qq.com>
2022-12-26 02:36:31 +0800

    Add @Mapper to every mapper to avoid ide errors

--- src/main/java/com/jokerxin/x509ca/mapper/LicenseMapper.java
+++ src/main/java/com/jokerxin/x509ca/mapper/LicenseMapper.java
@@ -2,7 +2,9 @@ package com.jokerxin.x509ca.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.jokerxin.x509ca.entity.License;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface LicenseMapper extends BaseMapper<License> {
 
 }
--- src/main/java/com/jokerxin/x509ca/mapper/RequestMapper.java
+++ src/main/java/com/jokerxin/x509ca/mapper/RequestMapper.java
@@ -2,7 +2,9 @@ package com.jokerxin.x509ca.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.jokerxin.x509ca.entity.Request;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface RequestMapper extends BaseMapper<Request> {
 
 }
