5d9f817e5c46db2fc3edf95f32984afab828a4fb
jokerxin <jxpro@qq.com>
2022-12-26 00:26:35 +0800

    Add cert util and then test

---
+++ src/main/java/com/jokerxin/x509ca/utils/CertUtil.java
@@ -0,0 +1,261 @@
+package com.jokerxin.x509ca.utils;
+
+import com.jokerxin.x509ca.entity.License;
+import com.jokerxin.x509ca.entity.Subject;
+import org.bouncycastle.asn1.x509.*;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CRLConverter;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509v2CRLBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.spec.ECParameterSpec;
+import org.bouncycastle.jce.spec.ECPublicKeySpec;
+import org.bouncycastle.math.ec.ECPoint;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+
+import javax.security.auth.x500.X500Principal;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.*;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509CRL;
+import java.security.cert.X509Certificate;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPublicKeySpec;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CertUtil {
+    private static final int ROOT_SIZE = 2048;
+    private static final String ROOT_SEED = "root12138";
+    private static final String ROOT_ALG = "RSA";
+    private static final String SIG_ALG = "SHA256withRSA";
+    private static final String PROVIDER = "BC";
+
+    private static final String CRL_URL = "https://www.example.com/crl";
+    private static final String ROOT_DN = "CN=SelfSign Root CA,O=HNU,OU=CS,C=CN,ST=海南省";
+    private static final DistributionPoint[] distributionPoints = new DistributionPoint[1];
+    private static final Date NotBefore = new Date(System.currentTimeMillis() - 1000L * 60 * 60 * 24);
+    private static final Date NotAfter = new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 365 * 10);
+
+    static {
+        // 添加BouncyCastle支持
+        Security.addProvider(new BouncyCastleProvider());
+        // 构造CRL distribution points扩展值
+        GeneralName generalName = new GeneralName(GeneralName.uniformResourceIdentifier, CRL_URL);
+        DistributionPointName distributionPointName = new DistributionPointName(new GeneralNames(generalName));
+        distributionPoints[0] = new DistributionPoint(distributionPointName, null, null);
+    }
+
+    /**
+     * 生成根CA的RSA密钥对
+     *
+     * @return keyPair 密钥对
+     */
+    public static KeyPair generateFixedKeyPair() throws Exception {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(ROOT_ALG, PROVIDER);
+        keyPairGenerator.initialize(ROOT_SIZE, new SecureRandom(ROOT_SEED.getBytes()));
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    /**
+     * 使用JcaX509v3CertificateBuilder生成根CA数字证书
+     */
+    public static void generateRootCert() throws Exception {
+        // 生成RSA密钥对
+        KeyPair keyPair = generateFixedKeyPair();
+        // 保存私钥
+        saveEncodedFile("root.privateKey", keyPair.getPrivate().getEncoded());
+        // 证书构造器
+        X509v3CertificateBuilder certGen = new JcaX509v3CertificateBuilder(
+                new X500Principal(ROOT_DN),
+                BigInteger.ONE,
+                new Timestamp(System.currentTimeMillis()),
+                NotAfter,
+                new X500Principal(ROOT_DN),
+                keyPair.getPublic())
+                .addExtension(Extension.keyUsage, true, new KeyUsage(KeyUsage.keyCertSign | KeyUsage.cRLSign))
+                .addExtension(Extension.basicConstraints, true, new BasicConstraints(true))
+                .addExtension(Extension.cRLDistributionPoints, false, new CRLDistPoint(distributionPoints));
+        // 签名构造器
+        ContentSigner sigGen = new JcaContentSignerBuilder(SIG_ALG).setProvider("BC").build(keyPair.getPrivate());
+        // 生成证书
+        X509Certificate cert = new JcaX509CertificateConverter().setProvider("BC").getCertificate(certGen.build(sigGen));
+        // 保存证书
+        saveEncodedFile("root.crt", cert.getEncoded());
+    }
+
+    /**
+     * 根据n和d生成RSA公钥
+     *
+     * @param n 模数n
+     * @param e 公钥e，一般为65537
+     * @return publicKey 公钥
+     */
+    public static PublicKey customRSAPublicKey(String n, String e) throws Exception {
+        // 构造RSA公钥参数RSAPublicKeySpec
+        RSAPublicKeySpec spec = new RSAPublicKeySpec(new BigInteger(n), new BigInteger(e));
+        // 使用RSAPublicKeySpec构造RSA公钥
+        return KeyFactory.getInstance(ROOT_ALG, PROVIDER).generatePublic(spec);
+    }
+
+    /**
+     * 根据参数生成ECC公钥
+     *
+     * @param curve ECC曲线名称（暂限三选一：secp256k1，sm2p256v1，prime256v1）
+     * @param x     ECPoint:X
+     * @param y     ECPoint:Y
+     * @return publicKey 公钥
+     */
+    public static PublicKey customECPublicKey(String curve, String x, String y) throws Exception {
+        // 根据params构造ECPublicKeyParameters
+        ECParameterSpec ecParameterSpec = ECNamedCurveTable.getParameterSpec(curve);
+        // 根据x和y构造ECPoint
+        ECPoint ecPoint = ecParameterSpec.getCurve().createPoint(new BigInteger(x), new BigInteger(y));
+        // 根据ECPublicKeyParameters和ECPoint构造ECPublicKeySpec
+        ECPublicKeySpec ecPublicKeySpec = new ECPublicKeySpec(ecPoint, ecParameterSpec);
+        // 使用ECPublicKeySpec构造ECC公钥
+        return KeyFactory.getInstance("EC", PROVIDER).generatePublic(ecPublicKeySpec);
+    }
+
+    /**
+     * 根据 Subject 实体生成 X500Principal 类型的 SubjectDN
+     *
+     * @param subject subject实体
+     * @return subjectDN
+     */
+    public static Map<String, Object> getSubjectDN(Subject subject, License license) {
+        Map<String, Object> map = new HashMap<>();
+        String DNString = "CN=" + subject.getCommonName() +
+                ",O=" + subject.getOrganization() +
+                ",OU=" + subject.getOrganizationalUnit() +
+                ",C=" + subject.getCountryName() +
+                ",ST=" + subject.getProvinceName();
+        GeneralName[] subjectAlternativeNames = new GeneralName[]{
+                new GeneralName(GeneralName.rfc822Name, subject.getEmail()),
+                new GeneralName(GeneralName.uniformResourceIdentifier, license.getLicenseUrl())
+        };
+        map.put("DNString", new X500Principal(DNString));
+        map.put("subjectAlternativeNames", new GeneralNames(subjectAlternativeNames));
+        return map;
+    }
+
+    /**
+     * 使用JcaX509v3CertificateBuilder生成用户数字证书
+     *
+     * @param subjectDN 使用者DN
+     * @param publicKey 用户自定义公钥
+     * @return X509Certificate证书
+     */
+    public static X509Certificate generateUserCert(Map<String, Object> subjectDN, PublicKey publicKey) throws Exception {
+        // 读取CA私钥
+        PrivateKey privateKey = readPrivateKey("root.privateKey", ROOT_ALG);
+        // 证书构造器
+        X509v3CertificateBuilder certGen = new JcaX509v3CertificateBuilder(
+                new X500Principal(ROOT_DN),
+                BigInteger.valueOf(System.currentTimeMillis()),
+                NotBefore,
+                NotAfter,
+                (X500Principal) subjectDN.get("DNString"),
+                publicKey)
+                .addExtension(Extension.keyUsage, true, new KeyUsage(KeyUsage.digitalSignature))
+                .addExtension(Extension.basicConstraints, false, new BasicConstraints(true))
+                .addExtension(Extension.cRLDistributionPoints, false, new CRLDistPoint(distributionPoints))
+                .addExtension(Extension.subjectAlternativeName, false, (GeneralNames) subjectDN.get("subjectAlternativeNames"));
+        // 签名构造器
+        ContentSigner sigGen = new JcaContentSignerBuilder(SIG_ALG).setProvider("BC").build(privateKey);
+        // 生成证书
+        return new JcaX509CertificateConverter().setProvider("BC").getCertificate(certGen.build(sigGen));
+    }
+
+    /**
+     * 将certificate转换为PEM格式
+     *
+     * @param certificate 证书
+     * @return PEM格式证书
+     */
+    public static String X509CertificateToPem(X509Certificate certificate) throws Exception {
+        StringWriter sw = new StringWriter();
+        JcaPEMWriter pemWriter = new JcaPEMWriter(sw);
+        pemWriter.writeObject(certificate);
+        pemWriter.close();
+        return sw.toString();
+    }
+
+    /**
+     * 生成CRL证书吊销列表
+     *
+     * @param serials 吊销的证书序列号
+     * @return X509CRL证书吊销列表
+     */
+    public static X509CRL generateCRL(BigInteger[] serials) throws Exception {
+        // 读取CA私钥
+        PrivateKey privateKey = readPrivateKey("root.privateKey", ROOT_ALG);
+        // 证书吊销列表构造器
+        JcaX509v2CRLBuilder crlGen = new JcaX509v2CRLBuilder(new X500Principal(ROOT_DN), new Date());
+        // 设置下次更新时间
+        crlGen.setNextUpdate(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24));
+        // 设置吊销证书序列号
+        for (BigInteger serial : serials) {
+            crlGen.addCRLEntry(serial, new Date(), CRLReason.affiliationChanged);
+        }
+        // 签名构造器
+        ContentSigner sigGen = new JcaContentSignerBuilder(SIG_ALG).setProvider("BC").build(privateKey);
+        // 生成证书吊销列表
+        return new JcaX509CRLConverter().setProvider("BC").getCRL(crlGen.build(sigGen));
+    }
+
+    /**
+     * 从文件中读取证书
+     *
+     * @param certPath 证书文件路径
+     * @return X509证书
+     */
+    public static X509Certificate readCert(String certPath) throws Exception {
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        return (X509Certificate) cf.generateCertificate(Files.newInputStream(Paths.get(certPath)));
+    }
+
+    /**
+     * 从文件中读取私钥
+     *
+     * @param privateKeyPath 私钥文件路径
+     * @param algorithm      签名算法
+     * @return 私钥
+     */
+    public static PrivateKey readPrivateKey(String privateKeyPath, String algorithm) throws Exception {
+        // 读取文件
+        FileInputStream fileInputStream = new FileInputStream(privateKeyPath);
+        byte[] bytes = new byte[fileInputStream.available()];
+        if (fileInputStream.read(bytes) == -1) {
+            throw new Exception("文件读取失败");
+        }
+        fileInputStream.close();
+        // 生成私钥
+        KeyFactory kf = KeyFactory.getInstance(algorithm, "BC");
+        return kf.generatePrivate(new PKCS8EncodedKeySpec(bytes));
+    }
+
+    /**
+     * 保存密钥或证书到文件
+     *
+     * @param filePath    文件路径
+     * @param encodedFile 编码后的内容
+     */
+    public static void saveEncodedFile(String filePath, byte[] encodedFile) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+        fileOutputStream.write(encodedFile);
+        fileOutputStream.close();
+    }
+}
--- src/test/java/com/jokerxin/x509ca/X509CaApplicationTests.java
+++ src/test/java/com/jokerxin/x509ca/X509CaApplicationTests.java
@@ -2,16 +2,24 @@ package com.jokerxin.x509ca;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.jokerxin.x509ca.entity.Demo;
+import com.jokerxin.x509ca.entity.License;
+import com.jokerxin.x509ca.entity.Subject;
 import com.jokerxin.x509ca.service.DemoService;
+import com.jokerxin.x509ca.utils.CertUtil;
 import com.jokerxin.x509ca.utils.HashUtil;
 import com.jokerxin.x509ca.utils.JWTUtil;
+import org.bouncycastle.jce.ECNamedCurveTable;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.sql.DataSource;
+import java.security.PublicKey;
+import java.security.cert.X509Certificate;
 import java.sql.SQLException;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class X509CaApplicationTests {
@@ -50,10 +58,56 @@ class X509CaApplicationTests {
     }
 
     @Test
-    public void testToken(){
+    public void testToken() {
         String token = JWTUtil.createToken(1, "admin");
         int id = JWTUtil.getUserId(token);
         System.out.println(token);
         System.out.println(id);
+    }
+
+    @Test
+    public void getAllCurveNames() {
+        // 加<?>是为了避免警告：Raw use of parameterized class 'Enumeration'
+        Enumeration<?> names = ECNamedCurveTable.getNames();
+        while (names.hasMoreElements()) {
+            String curveName = (String) names.nextElement();
+            System.out.println(curveName);
+        }
+    }
+
+    @Test
+    public void testCertUtil() throws Exception {
+        // 构建subjectDN
+        Subject subject = new Subject();
+        License license = new License();
+        subject.setCommonName("jokerxin");
+        subject.setOrganization("HNU");
+        subject.setOrganizationalUnit("CS");
+        subject.setCountryName("CN");
+        subject.setProvinceName("Hunan");
+        subject.setEmail("jokerxin@jokerxin.com");
+        license.setLicenseUrl("https://www.jokerxin.com/license");
+        Map<String, Object> subjectDN = CertUtil.getSubjectDN(subject, license);
+
+        // 生成根证书
+        CertUtil.generateRootCert();
+        // 根据参数生成RSA公钥
+        PublicKey rsaPublicKey = CertUtil.customRSAPublicKey("23942851064010346213536102616372514461758539530468226679046065021734636584436958829290455158509234895755974008162808739180500969653125868658784929835366184925346201664297960728004406986004492228252044475272828985983128450273474774737791946526690056881166146111833501827937227297535579777611430189634998227802759047528918991917960839552231005329621934011488364379817865804561318787840269819884365764673377924951839055210901678915668655362980762201745478607202046966346798420124342001447849391550311177768633493644311606465255087749009465995178524742197119605599822580797722544444894521358131954151142035293480046914131", "65537");
+        // 根据参数生成EC公钥
+        PublicKey ecPublicKey = CertUtil.customECPublicKey("prime256v1", "43402861010439923438948362831980934947833175921967755475820423341148777662561", "81787307312274621843680993027418882760249358609609586724278648046138262266910");
+        System.out.println("rsaPublicKey: " + rsaPublicKey);
+        System.out.println("ecPublicKey: " + ecPublicKey);
+
+        // 生成用户证书
+        X509Certificate rsaUserCert = CertUtil.generateUserCert(subjectDN, rsaPublicKey);
+        X509Certificate ecUserCert = CertUtil.generateUserCert(subjectDN, ecPublicKey);
+        // 保存用户证书
+        CertUtil.saveEncodedFile("user_rsa.pem", CertUtil.X509CertificateToPem(rsaUserCert).getBytes());
+        CertUtil.saveEncodedFile("user_ec.crt", ecUserCert.getEncoded());
+
+        // 读取证书并输出
+        System.out.println("root.crt: " +  CertUtil.readCert("root.crt"));
+        System.out.println("user_rsa.pem: " + CertUtil.readCert("user_rsa.pem"));
+        System.out.println("user_ec.crt: " + CertUtil.readCert("user_ec.crt"));
     }
 }
