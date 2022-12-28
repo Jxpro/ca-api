6dcf39863f957b507167bc808bd9c36a894decb1
jokerxin <jxpro@qq.com>
2022-12-28 20:35:15 +0800

    Update user controller

--- src/main/java/com/jokerxin/x509ca/controller/UserController.java
+++ src/main/java/com/jokerxin/x509ca/controller/UserController.java
@@ -2,6 +2,7 @@ package com.jokerxin.x509ca.controller;
 
 import com.jokerxin.x509ca.annotation.PassLogin;
 import com.jokerxin.x509ca.entity.User;
+import com.jokerxin.x509ca.http.Result;
 import com.jokerxin.x509ca.service.UserService;
 import com.jokerxin.x509ca.utils.HashUtil;
 import com.jokerxin.x509ca.utils.JWTUtil;
@@ -23,49 +24,45 @@ public class UserController {
 
     @PassLogin
     @PostMapping("/user/register")
-    public Map<String, Object> register(User user, HttpServletResponse response) {
-        Map<String, Object> map = new HashMap<>();
-        // 401 这里代表用户已经存在
-        map.put("code", 401);
-        // 判断用户名是否已存在，如果存在则返回错误信息，否则将用户信息存入数据库
-        if (userService.getByUsername(user.getUsername()) == null) {
+    public Result<Object> register(User user, HttpServletResponse response) {
+        if (userService.getByUsername(user.getUsername()) != null) {
+            return Result.unauthorized();
+        } else {
+            Map<String, Object> map = new HashMap<>();
             user.setPassword(HashUtil.sha256(user.getPassword().getBytes()));
             userService.saveUser(user);
-            map.put("code", 200);
             map.put("name", user.getNickname());
             map.put("role", user.getRole());
             response.setHeader("Authorization", JWTUtil.createToken(user.getId(), user.getUsername()));
             response.setHeader("Access-Control-Expose-Headers", "Authorization");
+            return Result.ok(map);
         }
-        return map;
     }
 
     @PassLogin
     @PostMapping("/user/login")
-    public Map<String, Object> login(@RequestParam String username,
-                                     @RequestParam String password,
-                                     HttpServletResponse response) {
-        Map<String, Object> map = new HashMap<>();
-        // 401 这里代表用户名或密码错误
-        map.put("code", 401);
-        // 获取用户信息
+    public Result<Object> login(@RequestParam String username,
+                                @RequestParam String password,
+                                HttpServletResponse response) {
         User user = userService.getByUsername(username);
-        if (Objects.equals(HashUtil.sha256(password.getBytes()), user.getPassword())) {
-            map.put("code", 200);
+        if (!Objects.equals(HashUtil.sha256(password.getBytes()), user.getPassword())) {
+            return Result.unauthorized();
+        } else {
+            Map<String, Object> map = new HashMap<>();
             map.put("name", username);
             map.put("role", user.getRole());
             response.setHeader("Authorization", JWTUtil.createToken(user.getId(), username));
             response.setHeader("Access-Control-Expose-Headers", "Authorization");
+            return Result.ok(map);
         }
-        return map;
     }
 
     @GetMapping("/user/info")
-    public Map<String, Object> userInfo(@RequestAttribute int userId) {
+    public Result<Object> userInfo(@RequestAttribute int userId) {
         Map<String, Object> map = new HashMap<>();
         User user = userService.getById(userId);
         map.put("name", user.getUsername());
         map.put("role", user.getRole());
-        return map;
+        return Result.ok(map);
     }
 }
